3818ce363bd767bd7a3c5b3327672c6d4eaf3a15
Rafael Silva <rafsilva004@gmail.com>
2023-06-22 20:09:50 -0500

    made final edits

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,12 +3,48 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 
+let basket = [];
+let bag = [1, 2, 3, 4];
+let cart = ['Bread', 'Eggs'];
 
 
 
+function addItem(item) {
+        basket.push(item);
+        return true;
+    }
 
+addItem('Apple');
+addItem('Bananas');
+addItem('Kale');
+addItem('Spinach');
 
+console.log(basket);
 
+  //- Create a function called `listItems`. It should:
+  //- loop over the items in the `basket` array
+  //- console.log each individual item on a new line
+
+  function listItems(array){
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+  }
+
+listItems(basket);
+listItems(bag);  
+listItems(cart);
+
+//- Create a function called `empty`. It should:
+//- reset the `basket` to an empty array
+
+function empty(array){
+    array.splice(0, array.length)
+}
+empty(basket);
+console.log(basket);
+
+// array.splice(0, array.length);
 
 
 
